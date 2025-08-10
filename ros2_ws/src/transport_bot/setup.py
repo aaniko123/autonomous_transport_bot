d2b1fb53f1b9989cdd49e56a085ba5c06e0367db
aaniko123 <adityaanikode@gmail.com>
2025-08-09 21:26:11 -0400

    Generated 4 wheel direct drive robot in Gazebo environment and made it move using velocity commands. ros2 topic pub -r 10 /cmd_vel geometry_msgs/msg/Twist '{linear: {x: 0.5}, angular: {z: 0.0}}'

--- ros2_ws/src/transport_bot/setup.py
+++ ros2_ws/src/transport_bot/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'transport_bot'
 
@@ -7,19 +9,22 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+    	('share/ament_index/resource_index/packages',
+        	['resource/' + package_name]),
+    	('share/' + package_name, ['package.xml']),
+    	(os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    	(os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='aditya',
     maintainer_email='aditya@todo.todo',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'follower_node = transport_bot.follower_node:main',
         ],
     },
 )
